fcfc2f322a3b5f1b12744d609f486dc2eeda1272
aarsilva <arthribeirosilva@gmail.com>
2019-07-29 22:24:56 -0300

    Build Commit

---
+++ build/precache-manifest.f4c18bd3c58d77f5dd0eaea3f5f16846.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bddc2e1dc74eda9847e9a8023a04a61b",
+    "url": "/b2w-challenge/index.html"
+  },
+  {
+    "revision": "2b0f0089efa5a429450f",
+    "url": "/b2w-challenge/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "2b0f0089efa5a429450f",
+    "url": "/b2w-challenge/static/js/2.66ebdc7e.chunk.js"
+  },
+  {
+    "revision": "85f4d577eb9d26713629",
+    "url": "/b2w-challenge/static/js/main.14d8d651.chunk.js"
+  },
+  {
+    "revision": "39403ab515347b0ff918",
+    "url": "/b2w-challenge/static/js/runtime~main.6651e104.js"
+  },
+  {
+    "revision": "31c8c115ac2c92421b494c004b020943",
+    "url": "/b2w-challenge/static/media/logo.31c8c115.svg"
+  },
+  {
+    "revision": "e536237ca1a521c232b0f9e20158783a",
+    "url": "/b2w-challenge/static/media/star-bkg.e536237c.png"
+  }
+]);
